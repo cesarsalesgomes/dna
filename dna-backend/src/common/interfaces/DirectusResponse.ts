8fa379233fc86e2b416fedf4d68831d748fab281
Cesar <cesarsalesgomes@gmail.com>
2021-08-08 20:07:49 -0300

    Migrating axios to graphql-request

---
+++ dna-backend/src/common/interfaces/DirectusResponse.ts
@@ -0,0 +1,5 @@
+interface DirectusResponse<T> {
+  data: T;
+}
+
+export default DirectusResponse;
--- dna-backend/src/system/interfaces/AuthenticationResponse.ts
+++
@@ -1,9 +0,0 @@
-interface AuthenticationResponse {
-  data: {
-    access_token: string;
-    refresh_token: string;
-    expires: number;
-  };
-}
-
-export default AuthenticationResponse;
