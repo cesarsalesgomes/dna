a2faca693213fe4199e3a1ca03fb1006a3c4e339
Cesar <cesarsalesgomes@gmail.com>
2021-09-19 23:18:04 -0300

    Created service to centralize methods common to spec files | Adjusted system spec file to use spec utils service | Created cat spec test to check cat controller succesfull instantiation

--- dna-backend/src/system/system.spec.ts
+++ dna-backend/src/system/system.spec.ts
@@ -1,66 +1,44 @@
-import { Test } from '@nestjs/testing';
 import {
   EnviromentVariableDirectusPortNotDefinedException,
   EnviromentVariableDockerDnaIPV4NotDefinedException,
   EnviromentVariableDockerDnaDirectusAccessTokenNotDefinedException,
 } from '@system/directus/directus.exception';
+import {
+  createTestingModuleOnInnerFunction,
+  setSpecSystemEnviromentVariables,
+} from '@utils/spec.utils';
 import { SystemModule } from './system.module';
 
 describe('[System] Unit Tests Spec', () => {
-  const processEnv = { ...process.env };
-
   it('Expected to throw EnviromentVariableDockerDnaIPV4NotDefined exception', async () => {
-    setSystemEnviromentVariables(false, false, false);
+    await setSpecSystemEnviromentVariables(false, false, false);
 
-    expect(createSystemModuleOnFunction()()).rejects.toThrowError(
-      EnviromentVariableDockerDnaIPV4NotDefinedException,
-    );
+    expect(
+      createTestingModuleOnInnerFunction([SystemModule])(),
+    ).rejects.toThrowError(EnviromentVariableDockerDnaIPV4NotDefinedException);
   });
 
   it('Expected to throw EnviromentVariableDirectusPortNotDefined exception', async () => {
-    setSystemEnviromentVariables(true, false, false);
+    await setSpecSystemEnviromentVariables(true, false, false);
 
-    expect(createSystemModuleOnFunction()()).rejects.toThrowError(
-      EnviromentVariableDirectusPortNotDefinedException,
-    );
+    expect(
+      createTestingModuleOnInnerFunction([SystemModule])(),
+    ).rejects.toThrowError(EnviromentVariableDirectusPortNotDefinedException);
   });
 
   it('Expected to throw EnviromentVariableDockerDnaIPV4NotDefined exception', async () => {
-    setSystemEnviromentVariables(true, true, false);
+    await setSpecSystemEnviromentVariables(true, true, false);
 
-    expect(createSystemModuleOnFunction()()).rejects.toThrowError(
+    expect(
+      createTestingModuleOnInnerFunction([SystemModule])(),
+    ).rejects.toThrowError(
       EnviromentVariableDockerDnaDirectusAccessTokenNotDefinedException,
     );
   });
 
-  it('Expected not to throw exception', async () => {
-    setSystemEnviromentVariables(true, true, true);
+  it('Expect to create system module without errors', async () => {
+    await setSpecSystemEnviromentVariables(true, true, true);
 
-    expect(createSystemModuleOnFunction()()).toBeDefined();
+    expect(createTestingModuleOnInnerFunction([SystemModule])()).toBeDefined();
   });
-
-  function createSystemModuleOnFunction() {
-    return async () => {
-      await Test.createTestingModule({
-        imports: [SystemModule],
-      }).compile();
-    };
-  }
-
-  function setSystemEnviromentVariables(
-    setIpv4Directus: boolean,
-    setDirectusPort: boolean,
-    setDirectusAccessToken: boolean,
-  ) {
-    resetEnviromentVariables();
-
-    if (setIpv4Directus) process.env.DOCKER_DNA_IPV4_DIRECTUS = 'defined';
-    if (setDirectusPort) process.env.DIRECTUS_PORT = 'defined';
-    if (setDirectusAccessToken)
-      process.env.DOCKER_DNA_DIRECTUS_ACCESS_TOKEN = 'defined';
-  }
-
-  function resetEnviromentVariables() {
-    process.env = { ...processEnv };
-  }
 });
