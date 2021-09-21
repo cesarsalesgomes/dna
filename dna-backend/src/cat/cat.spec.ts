910e4ac6d40fccfaa10cae153d1b95e6781e9392
Cesar <cesarsalesgomes@gmail.com>
2021-09-20 23:17:39 -0300

    Initial setup of integration test in Cat module

--- dna-backend/src/cat/cat.spec.ts
+++ dna-backend/src/cat/cat.spec.ts
@@ -1,25 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DirectusService } from '@system/directus/directus.service';
 import { setSpecSystemEnviromentVariables } from '@utils/spec.utils';
 import { CatController } from './cat.controller';
 import { CatModule } from './cat.module';
 
 describe('[Cat] Integration Tests Spec', () => {
-  let controller: CatController;
+  let module: TestingModule;
+  let catController: CatController;
+  let directusService: DirectusService;
 
   beforeAll(async () => {
     await setSpecSystemEnviromentVariables();
-    await setCatController();
+    await setModules([CatModule]);
+
+    setControllers();
+    setProviders();
   });
 
-  it('Cat controller defined', async () => {
-    expect(controller).toBeDefined();
+  it('Expect modules to be defined', async () => {
+    expect(module).toBeDefined();
   });
 
-  async function setCatController() {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [CatModule],
+  it('Expect controllers to be defined', async () => {
+    expect(catController).toBeDefined();
+  });
+
+  it('Expect providers to be defined', async () => {
+    expect(directusService).toBeDefined();
+  });
+
+  it('Expect route cats GET to return a list of cats', async () => {
+    jest
+      .spyOn(directusService.sdk, 'findAllCats')
+      .mockResolvedValue(<any>{ data: [] });
+
+    expect(await catController.findAll()).toBeDefined();
+  });
+
+  async function setModules(modules: Array<any>) {
+    module = await Test.createTestingModule({
+      imports: modules,
     }).compile();
+  }
 
-    controller = module.get<CatController>(CatController);
+  function setControllers() {
+    catController = module.get<CatController>(CatController);
+  }
+
+  function setProviders() {
+    directusService = module.get<DirectusService>(DirectusService);
   }
 });
