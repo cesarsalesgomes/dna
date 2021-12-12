515d4e7e82ce62d6932997daa7c400085d3082e2
Cesar <cesarsalesgomes@gmail.com>
2021-12-12 20:34:45 -0300

    Update on migration steps | Update on Heroku deployment of the Directus layer

--- dna-heroku/scripts/admin_role.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO public.directus_roles(
-	id, name, icon, description, enforce_tfa, admin_access, app_access
-) VALUES (
-	'437c1698-1299-4992-aebe-4d7562ca3a05', 
-	'Administrator', 
-	'verified', 
-	'Initial administrative role with unrestricted App/API access', 
-	false, true, true
-);
