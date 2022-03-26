42f88790e7234a139575b1275325168053f8f610
Cesar <cesarsalesgomes@gmail.com>
2022-03-26 11:29:47 -0300

    Removed axios dependency and added got dependency for typings purposes

--- nestjs/src/modules/restaurant-reservations/restaurant-reservations/restaurant-reservations.service.ts
+++ nestjs/src/modules/restaurant-reservations/restaurant-reservations/restaurant-reservations.service.ts
@@ -1,28 +1,23 @@
 import { TAG_ME_URL, TAG_ME_TOKEN, CASA_DO_PORCO_TAG_ME_ID } from '@constants/restaurant-service.constants';
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { AxiosResponse } from 'axios';
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import got from 'got';
+
+import { TagMeRestaurantInfo } from './restaurant-reservations.interfaces';
 
 @Injectable()
 export class RestaurantReservationsService {
-  constructor(private httpService: HttpService) { }
 
-  processGetTagMeRestauranteReservations() {
-    this.getTagMeRestauranteReservations(CASA_DO_PORCO_TAG_ME_ID).pipe(
-      map((data) => data),
-      take(1)
-    ).subscribe((data) => {
-      console.log(data);
-    });
+  getTagMeRestauranteReservations(idReservation: string) {
+    return got.get(
+      `${TAG_ME_URL}/ReservationStatus/${idReservation}/availabilityForApp/reservationWidget`,
+      {
+        headers: {
+          apitoken: TAG_ME_TOKEN
+        }
+      }).json<TagMeRestaurantInfo>();
   }
 
-  getTagMeRestauranteReservations(idReservation: string): Observable<AxiosResponse<any>> {
-    return this.httpService.get(`${TAG_ME_URL}/ReservationStatus/${idReservation}/availabilityForApp/reservationWidget`, {
-      headers: {
-        apitoken: TAG_ME_TOKEN
-      }
-    });
+  async processTagMeRestauranteReservations() {
+    const { availabilities } = await this.getTagMeRestauranteReservations(CASA_DO_PORCO_TAG_ME_ID);
   }
 }
