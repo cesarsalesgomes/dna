22be2c49f5e7e17187f01a1f6d6df4653f5ba49d
Cesar <cesarsalesgomes@gmail.com>
2022-06-24 12:10:46 -0300

    Removed access token from config | Refactored send login form hook

--- react/src/config/react-query.config.ts
+++ react/src/config/react-query.config.ts
@@ -3,6 +3,6 @@ export const endpointUrl = 'http://localhost/graphql';
 export const fetchParams = {
   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI5ZGY0ZTdhLTdlYjItNGZmMS04ZWJmLWZkYjFhNTI3ODc2YiIsInJvbGUiOiJkMjYxOTJiMC0xMzk4LTRhNTQtOTAxYi1jZGRiNTYzOGZlYTciLCJhcHBfYWNjZXNzIjp0cnVlLCJhZG1pbl9hY2Nlc3MiOnRydWUsImlhdCI6MTY1NTkyNDcyOSwiZXhwIjoxNjU2MDExMTI5LCJpc3MiOiJkaXJlY3R1cyJ9.HrM8C_aD48MC-cKh2rasVNFD6Sfy2kxRywVXFfp05cY'
+    Authorization: 'Bearer'
   }
 };
