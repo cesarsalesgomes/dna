e2769ff80f8cd2cec8f19da8f7a08864245161a5
Cesar <cesarsalesgomes@gmail.com>
2022-06-24 15:06:06 -0300

    Added access token in localstorage to avoid logging in again

--- react/src/features/auth/components/require-auth.component.tsx
+++ react/src/features/auth/components/require-auth.component.tsx
@@ -1,18 +1,20 @@
 import { Navigate, useLocation } from 'react-router-dom';
 
-import useAuth from '../hooks/auth.hook';
+import { getAccessToken, setAuthorizationHeader } from '../utils/token.utils';
 
 export default function RequireAuth({ children }: { children: JSX.Element }) {
-  const auth = useAuth();
   const location = useLocation();
+  const accessToken = getAccessToken();
 
-  if (!(auth?.authTokens?.auth_login?.access_token)) {
+  if (!(accessToken)) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
+
+  setAuthorizationHeader(accessToken);
 
   return children;
 }
--- react/src/features/auth/utils/token.utils.ts
+++ react/src/features/auth/utils/token.utils.ts
@@ -1,6 +1,24 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { fetchParams } from '@config/react-query.config';
 import { AuthLoginMutation } from '@hooks/auth.hooks';
 
+const accessTokenLocalStorageKey = 'dna-access-token';
+
+function setAccessTokenToLocalStorage(accessToken: string) {
+  localStorage.setItem(accessTokenLocalStorageKey, accessToken);
+}
+
+export function setAuthorizationHeader(accessToken: string) {
+  fetchParams.headers.Authorization = `Bearer ${accessToken}`;
+}
+
+export function getAccessToken() {
+  return localStorage.getItem(accessTokenLocalStorageKey);
+}
+
 export function setAccessToken(tokens: AuthLoginMutation) {
-  fetchParams.headers.Authorization = `Bearer ${tokens?.auth_login?.access_token}`;
+  const access_token = tokens?.auth_login?.access_token || '';
+
+  setAccessTokenToLocalStorage(access_token);
+  setAuthorizationHeader(access_token);
 }
