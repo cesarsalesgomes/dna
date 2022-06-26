ebe8f9226a66d45ed66b3c5d72f67be4766c9037
Cesar <cesarsalesgomes@gmail.com>
2022-06-26 10:09:54 -0300

    Added error handler hook inside react query custom fetcher

--- react/src/config/react-query.config.ts
+++ react/src/config/react-query.config.ts
@@ -1,7 +1,10 @@
+import { useErrorHandler } from 'react-error-boundary';
+
 /**
  * Custom fetchers used by React Query Codegen (https://www.graphql-code-generator.com/plugins/typescript-react-query)
  */
 
+// FIXME: store access token as a context hook (Example in the link above)
 export const fetchParams = {
   headers: {
     'Content-Type': 'application/json',
@@ -13,44 +16,54 @@ export const fetchParams = {
 
 const graphqlUrl = 'http://localhost/graphql';
 
-export function graphqlFetcher<TData, TVariables>(query: string, variables?: TVariables) {
-  return async (): Promise<TData> => {
+export const useGraphqlFetcher = <TData, TVariables>(
+  query: string, options?: RequestInit['headers']
+): ((variables?: TVariables) => Promise<TData>) => {
+  const handleError = useErrorHandler();
+
+  return async (variables?: TVariables): Promise<TData> => {
     const res = await fetch(graphqlUrl, {
       method: 'POST',
       ...(fetchParams),
+      ...(options ?? {}),
       body: JSON.stringify({ query, variables }),
     });
 
     const json = await res.json();
 
     if (json.errors) {
-      throw json.errors[0];
+      return handleError(json.errors[0]) as any;
     }
 
     return json.data;
   };
-}
+};
 
 // Graphql System Fetcher
 
 const graphqlSystemUrl = 'http://localhost/graphql/system';
 
-export function graphqlSystemFetcher<TData, TVariables>(query: string, variables?: TVariables) {
-  return async (): Promise<TData> => {
+export const useGraphqlSystemFetcher = <TData, TVariables>(
+  query: string, options?: RequestInit['headers']
+): ((variables?: TVariables) => Promise<TData>) => {
+  const handleError = useErrorHandler();
+
+  return async (variables?: TVariables): Promise<TData> => {
     const res = await fetch(graphqlSystemUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
+      ...(options ?? {}),
       body: JSON.stringify({ query, variables }),
     });
 
     const json = await res.json();
 
     if (json.errors) {
-      throw json.errors[0];
+      return handleError(json.errors[0]) as any;
     }
 
     return json.data;
   };
-}
+};
--- react/src/utils/error.utils.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-throw-literal */
-import GraphQLError from '../types/interfaces/graphql-error.interface';
-
-export function throwGraphQLErrorIfExists(error: GraphQLError | null) {
-  if (error) throw error;
-}
