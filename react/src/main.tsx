458acea4bc3e25e958159c6ce2810f5c9eda9d7b
Cesar <cesarsalesgomes@gmail.com>
2022-08-01 22:21:08 -0300

    Upgrade React 17 to 18

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
