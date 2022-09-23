ab21b6425e4082e2f9c2a9d572caa6156d2f5fae
Cesar <cesarsalesgomes@gmail.com>
2022-09-23 12:05:36 -0300

    Removed isFetching prop from ReacyQuery props to centralize reactions to fetching only on the loading component

--- react/src/features/cats/components/cat-list.component.tsx
+++ react/src/features/cats/components/cat-list.component.tsx
@@ -12,37 +12,34 @@ interface IShowCats extends ReactQueryProps<FindAllCatsQuery> {
   setCatId: Dispatch<SetStateAction<number>>
 }
 
-function ShowCats({ isFetching, data, setCatId }: IShowCats) {
+function ShowCats({ data, setCatId }: IShowCats) {
   return data && data?.cat ? (
-    <>
-      <div>
-        {data.cat.map((cat) => (
-          <p key={cat!.id}>
-            <button
-              type="button"
-              className="bg-cyan-800 p-2"
-              onClick={() => setCatId(Number(cat!.id))}
-              style={
-                // We can find the existing query data here to show bold links for ones that are cached
-                queryClient.getQueryData(['findAllCats', cat!.id])
-                  ? {
-                    backgroundColor: 'green',
-                  }
-                  : {}
-              }
-            >
-              {cat!.name}
-            </button>
-          </p>
-        ))}
-      </div>
-      <div>{isFetching ? 'Background Updating...' : ' '}</div>
-    </>
+    <div>
+      {data.cat.map((cat) => (
+        <p key={cat!.id}>
+          <button
+            type="button"
+            className="bg-cyan-800 p-2"
+            onClick={() => setCatId(Number(cat!.id))}
+            style={
+              // We can find the existing query data here to show bold links for ones that are cached
+              queryClient.getQueryData(['findAllCats', cat!.id])
+                ? {
+                  backgroundColor: 'green',
+                }
+                : {}
+            }
+          >
+            {cat!.name}
+          </button>
+        </p>
+      ))}
+    </div>
   ) : <div>No cats</div>;
 }
 
 function Cats({ setCatId }: { setCatId: Dispatch<SetStateAction<number>> }) {
-  const { status, data, isFetching } = useFindAllCatsQuery();
+  const { status, data } = useFindAllCatsQuery();
 
   return (
     <div className="prose flex flex-col dark:prose-invert">
@@ -51,7 +48,7 @@ function Cats({ setCatId }: { setCatId: Dispatch<SetStateAction<number>> }) {
       <div>
         {status === ReactQueryStatusEnum.Loading ? (
           'Loading...'
-        ) : <ShowCats isFetching={isFetching} data={data} setCatId={setCatId} />}
+        ) : <ShowCats data={data} setCatId={setCatId} />}
       </div>
     </div>
   );
--- react/src/features/cats/components/cat.component.tsx
+++ react/src/features/cats/components/cat.component.tsx
@@ -5,19 +5,18 @@ import { Dispatch, SetStateAction } from 'react';
 
 import useFindCatById from '../hooks/find-cat-by-id.hook';
 
-function ShowErrorOrCat({ status, error, isFetching, data }: ReactQueryProps<CatType>) {
+function ShowErrorOrCat({ status, error, data }: ReactQueryProps<CatType>) {
   return status === ReactQueryStatusEnum.Error ? (
     <span>Error: {error!.message}</span>
   ) : (
-      <div className="prose dark:prose-invert">
-        <h1>{data?.name}</h1>
-        <div>{isFetching ? 'Background Updating...' : ' '}</div>
-      </div>
-    );
+    <div className="prose dark:prose-invert">
+      <h1>{data?.name}</h1>
+    </div>
+  );
 }
 
 function Cat({ catId, setCatId }: { catId: number; setCatId: Dispatch<SetStateAction<number>> }) {
-  const { status, data, error, isFetching } = useFindCatById({ id: catId });
+  const { status, data, error } = useFindCatById({ id: catId });
 
   return (
     <div className="prose dark:prose-invert">
@@ -28,7 +27,7 @@ function Cat({ catId, setCatId }: { catId: number; setCatId: Dispatch<SetStateAc
       </div>
       {!catId || status === ReactQueryStatusEnum.Loading ? (
         'Loading...'
-      ) : <ShowErrorOrCat status={status} error={error} isFetching={isFetching} data={data} />}
+      ) : <ShowErrorOrCat status={status} error={error} data={data} />}
     </div>
   );
 }
