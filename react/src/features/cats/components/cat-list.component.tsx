f90fc10e4c2613d8c9d83e850c0b8bab87789b73
Cesar <cesarsalesgomes@gmail.com>
2022-10-09 19:36:57 -0300

    Adjusted navigate to login when forbidden error occurs | Added cat details route

--- react/src/features/cats/components/cat-list.component.tsx
+++ react/src/features/cats/components/cat-list.component.tsx
@@ -1,55 +1,48 @@
-import ReactQueryStatusEnum from '@enums/react-query-status.enum';
 import { FindAllCatsQuery, useFindAllCatsQuery } from '@hooks/cat.hooks';
-import ReactQueryProps from '@interfaces/react-query-props.interface';
 import QueryClientSingleton from '@providers/query-client.provider';
-import { Dispatch, SetStateAction } from 'react';
+import { Link } from 'react-router-dom';
 
 import CatCreate from './cat-create.component';
 
 const queryClient = QueryClientSingleton.getInstance();
 
-interface IShowCats extends ReactQueryProps<FindAllCatsQuery> {
-  setCatId: Dispatch<SetStateAction<number>>
-}
+function ShowCats({ data }: { data: FindAllCatsQuery }) {
+  const { cat } = data;
 
-function ShowCats({ data, setCatId }: IShowCats) {
-  return data && data?.cat ? (
-    <div>
-      {data.cat.map((cat) => (
-        <p key={cat!.id}>
-          <button
-            type="button"
-            className="bg-cyan-800 p-2"
-            onClick={() => setCatId(Number(cat!.id))}
-            style={
-              // We can find the existing query data here to show bold links for ones that are cached
-              queryClient.getQueryData(['findAllCats', cat!.id])
-                ? {
-                  backgroundColor: 'green',
-                }
-                : {}
-            }
-          >
-            {cat!.name}
-          </button>
-        </p>
+  return cat.length > 0 ? (
+    <>
+      {cat.map((item) => (
+        <div key={item!.id}>
+          <Link to={`/cats/${item.id}`}>
+            <button
+              type="button"
+              className='p-2'
+              style={
+                // We can find the existing query data here to show bold links for ones that are cached
+                queryClient.getQueryData(['findAllCats', item!.id])
+                  ? {
+                    backgroundColor: 'green',
+                  }
+                  : {}
+              }
+            >
+              {item!.name}
+            </button>
+          </Link>
+        </div>
       ))}
-    </div>
-  ) : <div>No cats</div>;
+    </>
+  ) : <h2>No cats</h2>;
 }
 
-function Cats({ setCatId }: { setCatId: Dispatch<SetStateAction<number>> }) {
-  const { status, data } = useFindAllCatsQuery();
+function Cats() {
+  const { data } = useFindAllCatsQuery();
 
   return (
     <div className="prose flex flex-col dark:prose-invert">
       <h1 className="my-4">Cats</h1>
       <CatCreate />
-      <div>
-        {status === ReactQueryStatusEnum.Loading ? (
-          'Loading...'
-        ) : <ShowCats data={data} setCatId={setCatId} />}
-      </div>
+      {data ? <ShowCats data={data} /> : <h2>No cats</h2>}
     </div>
   );
 }
--- react/src/features/login/utils/navigate-to-login.utils.ts
+++ react/src/features/login/utils/navigate-to-login.utils.ts
@@ -3,6 +3,5 @@ import { Location, NavigateFunction } from 'react-router-dom';
 export function navigateToLoginSettingStateToRedirectToPreviousPageAfterAuthenticating(
   navigate: NavigateFunction, location: Location
 ) {
-  // TODO: check how to navigate to root instead of parent
-  navigate('../login', { state: { from: location }, replace: true });
+  navigate('/login', { state: { from: location }, replace: true });
 }
