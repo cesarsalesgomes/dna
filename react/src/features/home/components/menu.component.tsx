480c263654290d6e7a7c1b812de51f24ec6dc465
Cesar <cesarsalesgomes@gmail.com>
2022-10-24 14:13:25 -0300

    Upgraded heroicons to v2

--- react/src/features/home/components/menu.component.tsx
+++ react/src/features/home/components/menu.component.tsx
@@ -1,6 +1,6 @@
 import { MENU_NAVBAR_HEIGHT } from '@constants/styles.constants';
 import { Disclosure, Menu as HeadlessUiMenu, Transition } from '@headlessui/react';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 
@@ -31,10 +31,10 @@ export default function Menu() {
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
