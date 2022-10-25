1f753dbd56c92ae71b75101e4946fbecf70a8bd0
Cesar <cesarsalesgomes@gmail.com>
2022-10-25 17:22:21 -0300

    Adjusted loading height when on suspense

--- react/src/components/loading/loading-layout.component.tsx
+++ react/src/components/loading/loading-layout.component.tsx
@@ -3,13 +3,15 @@ import { lazy } from 'react';
 
 const LoadingSpinner = lazy(() => import('@assets/looties/loading-spinner/loading-spinner.lootie'));
 
-export default function LoadingLayout() {
+export default function LoadingLayout({ isSuspense }: { isSuspense?: boolean }) {
   return (
     <div
       className="absolute z-50 flex w-screen items-center justify-center bg-black opacity-25"
-      style={{ height: `calc(100vh - ${MENU_NAVBAR_HEIGHT})` }}
+      style={{ height: `calc(100vh${isSuspense ? '' : ` - ${MENU_NAVBAR_HEIGHT}`})` }}
     >
       <LoadingSpinner style={{ height: '25%' }} />
     </div>
   );
 }
+
+LoadingLayout.defaultProps = { isSuspense: true };
