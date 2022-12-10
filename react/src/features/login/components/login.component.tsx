1c2dc5a17646cc3b108b057c429d434f050f0a18
Cesar <cesarsalesgomes@gmail.com>
2022-12-09 23:21:18 -0300

    Added Google SSO login on localhost

--- react/src/features/login/components/login.component.tsx
+++ react/src/features/login/components/login.component.tsx
@@ -84,6 +84,32 @@ export default function Login() {
 
             <SubmitButtonWithLoadingWhenFetching label="Sign in" />
           </form>
+
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="prose bg-gray-50 dark:bg-gray-900 px-2 dark:prose-invert ">Or continue with</span>
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-3 gap-3">
+              <div>
+                <a
+                  href="http://localhost:8055/auth/login/google?redirect=http://localhost:5173/login/google"
+                  className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                >
+                  <span className="sr-only">Sign in with Google</span>
+                  <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                    <path
+                      d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- react/src/routes/app.routes.tsx
+++ react/src/routes/app.routes.tsx
@@ -2,13 +2,13 @@ import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('@features/home/components/home.component'));
-const Login = lazy(() => import('@features/login/components/login.component'));
+const LoginRoutes = lazy(() => import('@features/login/routes/login.routes'));
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/*" element={<Home />} />
-      <Route path="/login/*" element={<Login />} />
+      <Route path="/login/*" element={<LoginRoutes />} />
     </Routes>
   );
 } 
