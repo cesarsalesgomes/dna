181658edaa27599b4bc3240301bf9ee6937bde5b
Cesar <cesarsalesgomes@gmail.com>
2023-07-08 20:17:24 -0300

    Import Eslint rules from React default project

--- sveltekit/.eslintrc.cjs
+++ sveltekit/.eslintrc.cjs
@@ -26,5 +26,43 @@ module.exports = {
         parser: '@typescript-eslint/parser'
       }
     }
-  ]
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'import/default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@/features/*/*'],
+      },
+    ],
+
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+
+    quotes: [2, 'single', { avoidEscape: true }],
+    'max-len': ['error', { code: 160, comments: 160, ignoreStrings: true }],
+    'no-var': 'error',
+    semi: 'error',
+    'no-multi-spaces': 'error',
+    'space-in-parens': 'error',
+    'no-multiple-empty-lines': 'error',
+    'prefer-const': 'error',
+    'no-use-before-define': 'error',
+    'import/prefer-default-export': 'off',
+  },
 };
