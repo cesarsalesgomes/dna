8b2e4e367ed6ad11325713f4a565419f4b80dbe2
Cesar <cesarsalesgomes@gmail.com>
2023-07-08 20:25:41 -0300

    Adjusted svelte eslint rules

--- sveltekit/.eslintrc.cjs
+++ sveltekit/.eslintrc.cjs
@@ -24,7 +24,18 @@ module.exports = {
       parser: 'svelte-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser'
-      }
+      },
+      rules: {
+        'svelte/indent': 'error',
+        indent: 'off',
+        'svelte/max-attributes-per-line': [
+          'error',
+          {
+            multiline: 1,
+            singleline: 3,
+          },
+        ],
+      },
     }
   ],
   rules: {
@@ -80,7 +91,7 @@ module.exports = {
     },
     'import/resolver': {
       typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
         project: './tsconfig.json',
       },
     },
