08501de8dacb0d7efa6b2a48d38fbf9ed2417024
Cesar <cesarsalesgomes@gmail.com>
2023-07-09 08:37:43 -0300

    Configured tailwind forms and typography | Configured Tailwind colors

--- sveltekit/tailwind.config.js
+++ sveltekit/tailwind.config.js
@@ -1,8 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      green: colors.green,
+      yellow: colors.yellow,
+      primary: colors.indigo,
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 };
