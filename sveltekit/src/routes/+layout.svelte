6a2ba881987c32acf09caaef3e37350b19a38f83
Cesar <cesarsalesgomes@gmail.com>
2023-07-19 18:34:23 -0300

    Added Menu navbar on home | Added Cats route

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script>
+  import Menu from '$components/Menu.svelte';
+  import { LOGIN_ROUTE } from '$constants/route.constants';
+
+  // eslint-disable-next-line import/extensions, import/no-unresolved
+  import { page } from '$app/stores';
+
   import './styles.css';
+
+  const onLoginRoute = $page.url.pathname === LOGIN_ROUTE;
 </script>
 
 <main class="h-screen bg-gray-50 dark:bg-gray-900">
-  <slot />
+  {#if onLoginRoute}
+    <slot/>
+  {:else}
+    <Menu/>
+    <div class="px-4 sm:px-12 md:px-24 lg:px-36 xl:px-48 2xl:px-60">
+      <slot />
+    </div>
+  {/if}
 </main>
---
+++ sveltekit/src/routes/cats/+page.svelte
@@ -0,0 +1,3 @@
+<div class="prose flex flex-col items-start dark:prose-invert">
+  <h1 class="my-4">Cats</h1>
+</div>
