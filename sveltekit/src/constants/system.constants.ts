5c9beab5354d6ce8d66a63df0d2bf6cd239d516c
Cesar <cesarsalesgomes@gmail.com>
2023-11-06 11:37:55 -0300

    Created new docker-compose file specific to Sveltekit requirements

--- sveltekit/src/constants/system.constants.ts
+++ sveltekit/src/constants/system.constants.ts
@@ -1,4 +1,4 @@
-export const DNA_BACKEND_URL = import.meta.env.PROD ? import.meta.env.VITE_DNA_API_URL : 'http://localhost';
+export const DNA_BACKEND_URL = import.meta.env.PROD ? import.meta.env.VITE_DNA_API_URL : 'http://localhost:8055';
 
 export const DNA_FRONT_URL = import.meta.env.PROD ? import.meta.env.VITE_DNA_FRONT_URL : 'http://localhost:5173';
 
